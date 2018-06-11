081c9b1d88ffc22f386bea7e26cc18c08b25c15e
Jonathan Ohara <jaraujo@squaretrade.com>
2018-06-11 11:39:52 -0300

    Filtering Wishlist

--- src/main/java/edu/jonathan/lookforcardprices/searchengine/service/shop/br/CHQShopService.java
+++ src/main/java/edu/jonathan/lookforcardprices/searchengine/service/shop/br/CHQShopService.java
@@ -34,7 +34,7 @@ public class CHQShopService extends SearchService {
 
 	@Override
 	protected ResultNameFilter getResultNameFilter() {
-		return ResultNameFilter.noFilter();
+		return ResultNameFilter.ignoreCaseContains();
 	}
 
 	@Override
